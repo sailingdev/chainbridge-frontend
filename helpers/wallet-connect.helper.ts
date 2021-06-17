a43c3381ade53888800cb2662a5a9d8bf2b309ae
sailingdev <sailingdev123@gmail.com>
2021-06-17 17:07:14 +0200

    wallet connect fix

--- helpers/wallet-connect.helper.ts
+++ helpers/wallet-connect.helper.ts
@@ -2,20 +2,18 @@ import { ethers } from "ethers";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import { mapSignerAsWallet } from "./wallet.helper";
 export const connect = async () => {
-    return new Promise((success, reject) => {
-        //  Create WalletConnect Provider
-        const walletProvider = new WalletConnectProvider({
-            infuraId: "27e484dcd9e3efcfd25a83a78777cdf1",
-        });
-        //  Enable session (triggers QR Code modal)
-        const accounts = await walletProvider.enable();
-        console.log('wallet provider enabled');
-        if (accounts.length > 0) {
-            const provider = new ethers.providers.Web3Provider(walletProvider)
-            const signer = provider.getSigner(accounts[0]);
-            return mapSignerAsWallet(signer);
-        } else {
-            throw new Error('No WalletConnect account retrieved');
-        }
+    //  Create WalletConnect Provider
+    const walletProvider = new WalletConnectProvider({
+        infuraId: "27e484dcd9e3efcfd25a83a78777cdf1",
+    });
+    //  Enable session (triggers QR Code modal)
+    const accounts = await walletProvider.enable();
+    console.log('wallet provider enabled');
+    if (accounts.length > 0) {
+        const provider = new ethers.providers.Web3Provider(walletProvider)
+        const signer = provider.getSigner(accounts[0]);
+        return mapSignerAsWallet(signer);
+    } else {
+        throw new Error('No WalletConnect account retrieved');
     }
 }
