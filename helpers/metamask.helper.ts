05bd8383296f8890edc657f81464451319518e42
sailingdev <sailingdev123@gmail.com>
2021-06-17 17:06:43 +0200

    Wallet Connect & MetaMask connections

--- helpers/metamask.helper.ts
+++ helpers/metamask.helper.ts
@@ -5,8 +5,14 @@ export const connect = async (): Promise<UserWallet> => {
     if (!(<any>window).ethereum) {
         throw new Error('MetaMask provider not available');
     }
-    const provider = new ethers.providers.Web3Provider((<any>window).ethereum)
-    const signer = provider.getSigner();
-    return mapSignerAsWallet(signer);
+    const { result } = await (<any>window).ethereum.send("eth_requestAccounts");
+    if (result?.length > 0) {
+        const provider = new ethers.providers.Web3Provider((<any>window).ethereum)
+        const signer = provider.getSigner(result[0]);
+        return mapSignerAsWallet(signer);
+    } else {
+        throw new Error('No MetaMask account retrieved');
+    }
 }
+
 
--- helpers/wallet-connect.helper.ts
+++ helpers/wallet-connect.helper.ts
@@ -2,25 +2,20 @@ import { ethers } from "ethers";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import { mapSignerAsWallet } from "./wallet.helper";
 export const connect = async () => {
-    //  Create WalletConnect Provider
-    const walletProvider = new WalletConnectProvider({
-        infuraId: "27e484dcd9e3efcfd25a83a78777cdf1",
-        // bridge: process.env.NEXT_PUBLIC_BRIDGE_ROPSTEN_ADDRESS
-    });
-    // Subscribe to chainId change
-    walletProvider.on("chainChanged", (chainId: number) => {
-        console.log(chainId);
-    });
-    // Subscribe to session disconnection
-    walletProvider.on("disconnect", (code: number, reason: string) => {
-        console.log(code, reason);
-    });
-    await walletProvider.enable();
-    //  Enable session (triggers QR Code modal)
-    walletProvider.on("accountsChanged", (accounts: string[]) => {
-        console.log(accounts);
-    });
-    const provider = new ethers.providers.Web3Provider(walletProvider)
-    const signer = provider.getSigner();
-    return mapSignerAsWallet(signer);
+    return new Promise((success, reject) => {
+        //  Create WalletConnect Provider
+        const walletProvider = new WalletConnectProvider({
+            infuraId: "27e484dcd9e3efcfd25a83a78777cdf1",
+        });
+        //  Enable session (triggers QR Code modal)
+        const accounts = await walletProvider.enable();
+        console.log('wallet provider enabled');
+        if (accounts.length > 0) {
+            const provider = new ethers.providers.Web3Provider(walletProvider)
+            const signer = provider.getSigner(accounts[0]);
+            return mapSignerAsWallet(signer);
+        } else {
+            throw new Error('No WalletConnect account retrieved');
+        }
+    }
 }
